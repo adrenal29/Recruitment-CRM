b763968e5f6d14d5582d077fcf41499f29489ff5
adrenal29 <mohit29code@gmail.com>
2022-08-27 12:38:17 -0700

    landing page crm

--- server.js
+++ server.js
@@ -1,17 +1,31 @@
 const express=require('express');
 const app=express();
-app.get('',(req,res)=>{
-    res.send('Hello express');
-})
+const bodyParser=require('body-parser');
+var request = require('request');
+// app.get('',(req,res)=>{
+//     res.send('Hello express');
+// })
 app.get('/home',(req,res)=>{
     res.sendFile(__dirname+"/index.html");
+})
+app.get('/',(req,res)=>{
+  res.sendFile(__dirname+"/views/home.html");
+})
+app.get('/builder',(req,res)=>{
+  res.sendFile(__dirname+"/views/builder.html");
+})
+app.get('/recruit',(req,res)=>{
+  res.sendFile(__dirname+"/views/recruit.html");
 })
 app.listen('4000',()=>{
     console.log("server is listening on port 4000");
 })
 app.use(express.static(__dirname));
+app.use(express.static(__dirname+'/views'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.post('/add',(req,res)=>{
+res.send('User name is:'+req.body.username);
 
-var request = require('request');
 var options = {
   'method': 'POST',
   'url': 'https://local-1-29sharma.harperdbcloud.com',
@@ -20,8 +34,11 @@ var options = {
     'Authorization': "Basic " + Buffer.from('mohit'+ ':' + 'mohit29#').toString('base64')
   },
   body: JSON.stringify({
-    "operation": "create_schema",
-    "schema": "dev"
+    "operation": "add_user",
+    "role": "super_user",
+    "username": req.body.username,
+    "password": req.body.pwd,
+    "active": true
 })
 
 };
@@ -29,27 +46,23 @@ request(options, function (error, response) {
   if (error) throw new Error(error);
   console.log(response.body);
 });
+})
 
-app.get('/send',(req,res)=>{
-    var options = {
-        'method': 'POST',
-        'url': 'https://local-1-29sharma.harperdbcloud.com',
-        'headers': {
-          'Content-Type': 'application/json',
-          'Authorization': "Basic " + Buffer.from('mohit'+ ':' + 'mohit29#').toString('base64')
-        },
-        body: JSON.stringify({
-          "operation": "add_user",
-          "role": "super_user",
-          "username": "hdb_user",
-          "password": "password",
-          "active": true
-      })
-      
-      };
-      request(options, function (error, response) { 
-        if (error) throw new Error(error);
-        console.log(response.body);
-      });
-      res.send("hi")
+app.get('/info',(req,res)=>{
+  var options = {
+    'method': 'POST',
+    'url': 'https://local-1-29sharma.harperdbcloud.com',
+    'headers': {
+      'Content-Type': 'application/json',
+      'Authorization': "Basic " + Buffer.from('mohit'+ ':' + 'mohit29#').toString('base64')
+    },
+    body: JSON.stringify({
+      "operation": "user_info"
+  })
+  
+  };
+  request(options, function (error, response) { 
+    if (error) throw new Error(error);
+    console.log(response.body);
+  });
 })
