89048706c2d80fab5b221b597364b64d32381742
adrenal29 <mohit29code@gmail.com>
2022-08-29 02:29:36 -0700

    added feature/ refactoring left

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express=require('express');
 const app=express();
 const bodyParser=require('body-parser');
+const sessionStorage=require('sessionstorage')
 var request = require('request');
+
 // app.get('',(req,res)=>{
 //     res.send('Hello express');
 // })
@@ -15,7 +17,40 @@ app.get('/builder',(req,res)=>{
   res.sendFile(__dirname+"/views/builder.html");
 })
 app.get('/recruit',(req,res)=>{
+  res.sendFile(__dirname+"/views/recruit.html")
+})
+app.get('/r/:id',(req,res)=>{
   res.sendFile(__dirname+"/views/recruit.html");
+  var request = require('request');
+var options = {
+  'method': 'POST',
+  'url': 'https://local-1-29sharma.harperdbcloud.com',
+  'headers': {
+    'Content-Type': 'application/json',
+    'Authorization': "Basic " + Buffer.from('mohit'+ ':' + 'mohit29#').toString('base64')
+  },
+  body: JSON.stringify({
+    "operation": "search_by_value",
+    "schema": "users",
+    "table": "recruiter",
+    "search_attribute": "email",
+    "search_value": "mohit",
+    "get_attributes": [
+        "email",
+        "password"
+    ]
+})
+
+};
+request(options, function (error, response) { 
+  if (error) throw new Error(error);
+  var x=response.body;
+  console.log(x)
+  
+});
+})
+app.get('/candidates',(req,res)=>{
+  res.sendFile(__dirname+"/views/test.html");
 })
 app.get('/login',(req,res)=>{
   res.sendFile(__dirname+"/views/login.html");
@@ -25,6 +60,7 @@ app.listen('4000',()=>{
 })
 app.use(express.static(__dirname));
 app.use(express.static(__dirname+'/views'));
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.post('/add',(req,res)=>{
 res.send('User name is:'+req.body.username);
@@ -53,6 +89,7 @@ request(options, function (error, response) {
 
 app.post('/dashboard',(req,res)=>{
   res.sendFile(__dirname+"/views/dashboard.html");
+  sessionStorage.setItem("user",req.body.username)
   var options = {
     'method': 'POST',
     'url': 'https://local-1-29sharma.harperdbcloud.com',
@@ -67,7 +104,8 @@ app.post('/dashboard',(req,res)=>{
       "records": [
           {   
               "email":req.body.username,
-              "password":req.body.pwd
+              "password":req.body.pwd,
+              "jobs":0
           }
       ]
   })
